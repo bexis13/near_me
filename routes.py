61f9d8487a7870bb42099b4f240d4e21d743743b
aboseu <bexis17@gmail.com>
2016-11-10 13:45:52 +0000

    initial commit

---
+++ routes.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+@app.route('/about')
+def about():
+    return render_template('about.html')
+    
+if __name__=="__main__":   
+    app.run(debug= True,host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+
+# import os
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello():
+#     return 'test is running fine'
+
+# app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+    
---
+++ static/img/file.py
@@ -0,0 +1,6 @@
+# import requests
+
+# image = requests.get('http://betanews.com/wp-content/uploads/2013/10/MBP15_PF_iBooks_Maps_PRINT-600x347.jpg', stream=True)#make a request to get the photo
+# image_file = open("image_file.jpg","wb") #wb is for writting to the file in binary mode. only w is for text files only
+# image_file.write(image.content)
+# image_file.close()
