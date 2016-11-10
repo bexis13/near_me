b3369c0a450f5639b15706788d50c9fc6d91b12b
aboseu <bexis17@gmail.com>
2016-11-10 13:59:53 +0000

    remove unused commented code from route py, and add helpful comment

--- routes.py
+++ routes.py
@@ -11,17 +11,7 @@ def index():
 def about():
     return render_template('about.html')
     
+#still in development, this is development port and ip for cloud9 workspace   
 if __name__=="__main__":   
     app.run(debug= True,host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
 
-# import os
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def hello():
-#     return 'test is running fine'
-
-# app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
-    
